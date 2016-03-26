2989daed16db38c3c3f8fc74a3e2cbf6d7c15e53
Marius <chmdebeer@gmail.com>
2016-03-25 23:50:51 -0700

    Enabled heatbed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -484,11 +484,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // Preheat Constants
 #define PLA_PREHEAT_HOTEND_TEMP 210 
-#define PLA_PREHEAT_HPB_TEMP 0
+#define PLA_PREHEAT_HPB_TEMP 50
 #define PLA_PREHEAT_FAN_SPEED 165   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 245
-#define ABS_PREHEAT_HPB_TEMP 0
+#define ABS_PREHEAT_HPB_TEMP 90
 #define ABS_PREHEAT_FAN_SPEED 165   // Insert Value between 0 and 255
 
 
